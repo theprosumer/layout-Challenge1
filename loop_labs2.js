c22d153de6a2f26e5d04ead64e86ed72b95bd666
John Favors <johnmfavors@gmail.com>
2017-02-06 08:52:43 -0600

    added to homework
    forgot to add

---
+++ loop_labs2.js
@@ -0,0 +1,35 @@
+/* write a loop that will iterate from
+50 to 20. for each iteration if the current
+number is a multiple of 3, console log the value*/
+
+for (var i = 50;  i < 21;  i--) {
+	if (i / 3 === 0)
+		console.log(i)
+}
+
+/*write an infinte loop*/
+while (true) {
+
+}
+
+/*loop through this array and print
+every other value*/
+var classmates = ["Bridjet", "Nathan", "Matias", "Mike", "Jason", "Reuben", "Ben", "Lane", "John", "Chris", "Terry", "Ann", "Katelyn", "Naomi", "Jeff"];
+for (var i = 1; i = classmates.length; i++) {
+	if [] % 2 === 0
+		console.log(classmates)
+}
+
+/*Create a new array with the glten-free
+options from this menu*/
+var glutenFreeMenu = []
+var menu = [{name: "pizza", glutenFree: false},{name: "salad", glutenFree: true},{name: "donut", glutenFree: false},{name: "steak", glutenFree: true},{name: "chicken", glutenFree: true},{name: "cheeseburger", glutenFree: false}];
+	for (var glutenFree === true in menu)
+		console.log(name)
+
+/*Create a new array with all the players
+that have a true value of goat (greatest
+of all time) from each league*/
+var allTimeGreatest = []
+var league = [{name: "NFL", players: [{name: "Tom Brady", goat: false}, {name: "Walter Payton", goat: true}]}, {name: "NBA", players: [{name: "Michael Jordan", goat: true}, {name: "Lebron James", goat: false}]},{name: "MLB", players: [{name: "Babe Ruth", goat: true}, {name: "Alex Rodriguez", goat: false}]}];
+	for(var )
